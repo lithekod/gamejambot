5e9b709897f51a75ed0dfc58523a5b43528d5bdb
TheZoq2 <frans.skarman@protonmail.com>
2020-04-14 22:19:03 +0200

    Add all the features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use std::{env, error::Error};
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::sync::Mutex;
+use std::collections::HashMap;
+use std::path::PathBuf;
+
 use tokio::stream::StreamExt;
+use serde_derive::{Serialize, Deserialize};
+use anyhow::Context;
+use lazy_static::lazy_static;
+use serde_json;
+use regex::Regex;
 
 use twilight::{
     cache::{
@@ -12,12 +23,68 @@ use twilight::{
     model::{
         gateway::GatewayIntents,
         user::CurrentUser,
-        channel::Channel,
-        id::ChannelId,
+        channel::{Message, Channel, ChannelType, GuildChannel},
+        id::{ChannelId, UserId, GuildId},
     },
 };
 
-type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
+type Result<T> = std::result::Result<T, anyhow::Error>;
+
+enum SubmissionResult {
+    Done,
+    AlreadySubmitted,
+}
+
+const FILENAME: &'static str = "themes.json";
+
+#[derive(Serialize, Deserialize)]
+struct ThemeIdeas {
+    content: HashMap<UserId, String>,
+}
+
+impl ThemeIdeas {
+    fn load() -> Result<Self> {
+        if PathBuf::from(FILENAME).exists() {
+            let mut file = File::open(FILENAME)?;
+            let mut content = String::new();
+            file.read_to_string(&mut content)?;
+            Ok(serde_json::from_str(&content)?)
+        }
+        else {
+            Ok(Self {content: HashMap::new()})
+        }
+    }
+
+    pub fn instance() -> &'static Mutex<Self> {
+        lazy_static! {
+            static ref INSTANCE: Mutex<ThemeIdeas> = Mutex::new(
+                ThemeIdeas::load().unwrap()
+            );
+        }
+        &INSTANCE
+    }
+
+    pub fn try_add(&mut self, user: UserId, idea: &str) -> Result<SubmissionResult> {
+        if self.content.contains_key(&user) {
+            self.content.insert(user, idea.into());
+            self.save().context("Failed to write current themes")?;
+            Ok(SubmissionResult::AlreadySubmitted)
+        }
+        else {
+            self.content.insert(user, idea.into());
+            self.save().context("Failed to write current themes")?;
+            Ok(SubmissionResult::Done)
+        }
+    }
+
+    pub fn save(&self) -> Result<()> {
+        let mut file = File::create(FILENAME)
+            .with_context(|| format!("failed to open {} for writing", FILENAME))?;
+        file.write_all(serde_json::to_string(&self)?.as_bytes())
+            .with_context(|| format!("failed to write to {}", FILENAME))?;
+        Ok(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -95,15 +162,28 @@ async fn handle_event(
                             .await?;
                     }
                     else {
-                        // Check if the message is a PM
-                        http.create_message(msg.channel_id)
-                            .content("Theme idea registered, thanks!")
-                            .await?;
+                        let has_old_theme = ThemeIdeas::instance().lock().unwrap()
+                            .try_add(msg.author.id, &msg.content)
+                            .context("failed to save theme")?;
+
+                        match has_old_theme {
+                            SubmissionResult::Done => {
+                                // Check if the message is a PM
+                                http.create_message(msg.channel_id)
+                                    .content("Theme idea registered, thanks!")
+                                    .await?;
+                            }
+                            SubmissionResult::AlreadySubmitted => {
+                                // Check if the message is a PM
+                                http.create_message(msg.channel_id)
+                                    .content("You can only send one idea. We replaced your old submission")
+                                    .await?;
+                            }
+                        }
                     }
                 }
                 else {
-                    http.create_message(msg.channel_id)
-                        .content("not a pm")
+                    handle_potential_command(&msg, http, current_user)
                         .await?;
                 }
             }
@@ -116,3 +196,105 @@ async fn handle_event(
 
     Ok(())
 }
+
+async fn handle_potential_command(
+    msg: &Message,
+    http: HttpClient,
+    current_user: &CurrentUser
+) -> Result<()> {
+    let mut words = msg.content.split_ascii_whitespace();
+    match words.next() {
+        Some("~help") => {
+            send_help_message(msg.channel_id, http).await?;
+        }
+        Some("~create_channel") => {
+            handle_create_channel(
+                &words.collect::<Vec<_>>(),
+                msg.channel_id,
+                msg.guild_id.expect("Tried to create channel in non-guild"),
+                http
+            ).await?;
+        },
+        Some(s) if s.chars().next() == Some('~') => {
+            http.create_message(msg.channel_id)
+                .content("Unrecognised command")
+                .await?;
+            send_help_message(msg.channel_id, http).await?;
+        }
+        // Not a command and probably not for us
+        Some(_) => {
+            // Check if we were mentioned
+            if msg.mentions.contains_key(&current_user.id) {
+                send_help_message(msg.channel_id, http).await?;
+            }
+        }
+        None => {}
+    }
+    Ok(())
+}
+
+async fn send_help_message(
+    channel_id: ChannelId,
+    http: HttpClient,
+) -> Result<()> {
+    http.create_message(channel_id)
+        .content("Talk to me in a PM to submit theme ideas.\n\nYou can also ask for a voice channel by sending `~create_channel <channel name>`")
+        .await?;
+    Ok(())
+}
+
+
+async fn handle_create_channel<'a>(
+    rest_command: &[&'a str],
+    original_channel: ChannelId,
+    guild: GuildId,
+    http: HttpClient
+) -> Result<()> {
+    lazy_static! {
+        static ref VALID_REGEX: Regex = Regex::new("[_A-z0-9]+").unwrap();
+    }
+    println!("got request for channel with name {:?}", rest_command);
+    let reply = if rest_command.len() == 0 {
+        "You need to specify a team name".to_string()
+    }
+    else if rest_command.len() > 1 {
+        "Channel names can not contain whitespace".into()
+    }
+    // Check if the name is valid
+    else if !VALID_REGEX.find_iter(rest_command[0])
+            // The regex crate does not have a function to check if the whole string
+            // matches the regex. Instead we check if any of the matches
+            // are the same as the whole string being searched
+            .any(|m| m.as_str() == rest_command[0])
+    {
+        "Channel names can only contain A-z, _ and digits".into()
+    }
+    else {
+        let request = http.create_guild_channel(guild, rest_command[0])
+            .kind(ChannelType::GuildVoice)
+            .nsfw(true);
+        match request.await {
+            Ok(GuildChannel::Voice(_)) => {
+                "Channel created 🎊".into()
+            }
+            Ok(_) => {
+                "A channel was created but it wasn't a voice channel 🤔. Blame discord".into()
+            }
+            Err(e) => {
+                println!(
+                    "Failed to create channel {}. Error: {:?}",
+                    rest_command[0],
+                    e
+                );
+                "Channel creation failed, check logs for details".into()
+            }
+        }
+    };
+
+    http.create_message(original_channel)
+        .content(&reply)
+        .await?;
+
+    Ok(())
+}
+
