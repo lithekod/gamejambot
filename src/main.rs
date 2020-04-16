8e25feb4fb9dbd92f18f1d03e9851769227c8e08
Robin Sliwa <rorrosliwa@gmail.com>
2020-04-16 16:03:48 +0200

    A little nicer console prints when giving/removing roles

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ use twilight::{
     http::error::Error as DiscordError,
     model::{
         gateway::GatewayIntents,
-        user::CurrentUser,
+        user::{User, CurrentUser},
         channel::{Message, Channel, ChannelType, GuildChannel},
         id::{ChannelId, UserId, GuildId},
     },
@@ -291,7 +291,7 @@ async fn handle_potential_command(
                 &words.collect::<Vec<_>>(),
                 msg.channel_id,
                 msg.guild_id.expect("Tried to create channel in non-guild"),
-                msg.author.id,
+                &msg.author,
                 http
             ).await?;
         },
@@ -300,7 +300,7 @@ async fn handle_potential_command(
                 &words.collect::<Vec<_>>(),
                 msg.channel_id,
                 msg.guild_id.expect("Tried to create channel in non-guild"),
-                msg.author.id,
+                &msg.author,
                 http
             ).await?;
         },
@@ -495,7 +495,7 @@ async fn handle_give_role<'a>(
     rest_command: &[&'a str],
     original_channel: ChannelId,
     guild: GuildId,
-    msg_author_id: UserId,
+    author: &User,
     http: HttpClient
 ) -> Result<()> {
     let mut message = "You need to to specify a valid role.\nAvailable roles are:```Programmer\n2D Artist\n3D Artist\nSound Designer\nMusician\nIdea Guy\nBoard Games```".to_string();
@@ -508,15 +508,15 @@ async fn handle_give_role<'a>(
 
         for role in guild_roles {
             if role.name.to_lowercase() == rest_command.join(" ").to_lowercase() {
-                let request = http.add_guild_member_role(guild, msg_author_id, role.id);
+                let request = http.add_guild_member_role(guild, author.id, role.id);
 
                 match request.await {
                     Ok(_) => {
                         message = format!("You have been assigned the role **{}**.", role.name);
-                        println!("New role {} assigned to {}", role.name, msg_author_id);
+                        println!("New role {} assigned to {}", role.name, author.name);
                     }
                     Err(e) => {
-                        println!("Couldn't assign role {} to {}\n{}", role.name, msg_author_id, e);
+                        println!("Couldn't assign role {} to {}\n{}", role.name, author.name, e);
                     }
                 }
             }
@@ -525,7 +525,7 @@ async fn handle_give_role<'a>(
     };
 
     http.create_message(original_channel)
-        .content(format!("<@{}> {}", msg_author_id, reply))
+        .content(format!("<@{}> {}", author.id, reply))
         .await?;
 
     Ok(())
@@ -535,7 +535,7 @@ async fn handle_remove_role<'a>(
     rest_command: &[&'a str],
     original_channel: ChannelId,
     guild: GuildId,
-    msg_author_id: UserId,
+    author: &User,
     http: HttpClient
 ) -> Result<()> {
     let mut message = "You need to to specify a valid role.\nAvailable roles are:```Programmer\n2D Artist\n3D Artist\nSound Designer\nMusician\nIdea Guy\nBoard Games```".to_string();
@@ -548,15 +548,15 @@ async fn handle_remove_role<'a>(
 
         for role in guild_roles {
             if role.name.to_lowercase() == rest_command.join(" ").to_lowercase() {
-                let request = http.remove_guild_member_role(guild, msg_author_id, role.id);
+                let request = http.remove_guild_member_role(guild, author.id, role.id);
 
                 match request.await {
                     Ok(_) => {
                         message = format!("You have been stripped of the role **{}**.", role.name);
-                        println!("{} left the role {}", msg_author_id, role.name);
+                        println!("{} left the role {}", author.name, role.name);
                     }
                     Err(e) => {
-                        println!("Couldn't remove role {} from {}\n{}", role.name, msg_author_id, e);
+                        println!("Couldn't remove role {} from {}\n{}", role.name, author.name, e);
                     }
                 }
             }
@@ -565,7 +565,7 @@ async fn handle_remove_role<'a>(
     };
 
     http.create_message(original_channel)
-        .content(format!("<@{}> {}", msg_author_id, reply))
+        .content(format!("<@{}> {}", author.id, reply))
         .await?;
 
     Ok(())
