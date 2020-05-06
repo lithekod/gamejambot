7c01996f24166b4f65f87b4264128da13bf7ee20
Axel Nordanskog <axeno840@student.liu.se>
2020-05-06 11:54:31 +0200

    Prevent crashing from lacking send permissions

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,13 @@
 use std::fmt::Display;
 
 use twilight::{
-    http::Client as HttpClient,
+    http::{
+        Client as HttpClient,
+        error::{
+            Error as DiscordError,
+            ResponseError,
+        },
+    },
     model::id::{ChannelId, UserId},
 };
 
@@ -13,8 +19,20 @@ pub async fn send_message(
     user_id: UserId,
     content: impl Into<String> + Display,
 ) -> Result<()> {
-    http.create_message(channel_id)
+    let context = "send_message";
+    match http.create_message(channel_id)
         .content(format!("<@{}> {}", user_id, content))
-        .await?;
+        .await {
+        Err(DiscordError::Response{source: ResponseError::Client{response: r}}) => {
+            println!("{}: The response was a client side error: {}", context,
+                match r.text().await {
+                    Ok(text) => text,
+                    _ => "(Response unavailable)".to_string(),
+                }
+            );
+        },
+        Err(e) => println!("{}: The response was an unknown error: {:?}", context, e),
+        _ => {}
+    };
     Ok(())
 }
